34e2825c2e926c91fed4f1e7a5e1134abe680c4a
Nunez Dorian <doriannunez64@gmail.com>
2021-12-13 12:43:16 +0100

    voyons

--- src/Jour9/partie1.java
+++ src/Jour9/partie1.java
@@ -10,35 +10,6 @@ public class partie1 {
         Scanner ligne = new Scanner(doc);
 
         List<Integer> listePrecedente = new ArrayList<>();
-        List<Integer> listeActuelle = new ArrayList<>();
-        List<Integer> listeFuture = new ArrayList<>();
-
-
-        String[] chiffrePrecedent = ligne.nextLine().split("");
-        for(String c:chiffrePrecedent)
-        {
-            listeActuelle.add(Integer.parseInt(c));
-            
-        }
-        String[] chiffreAcuel = ligne.nextLine().split("");
-        for(String c:chiffreAcuel)
-        {
-            listeFuture.add(Integer.parseInt(c));
-            
-        }
-
-        int indexSuperieur =0;
-        for (int i = 0 ; i <listeActuelle.size(); i++)
-        {
-            indexSuperieur++;
-            int chiffreApres = listeActuelle.get(indexSuperieur);
-            int chiffreActuel = listeActuelle.get(i);
-            int chiffreDessous = listeFuture.get(i);
-
-            /* Faire en sorte qu'on ne sorte pas de la liste */
-            
-            
-        }
         
 
         System.out.println(listeActuelle);
